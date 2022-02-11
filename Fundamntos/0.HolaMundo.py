b9350ff268fa14a25f8a4e2b122d2359c84f16b5
Mario <mecuevaa@gmail.com>
2022-02-11 11:17:43 -0500

    inicio

--- Fundamntos/0.HolaMundo.py
+++ Fundamntos/0.HolaMundo.py
@@ -1,1 +1,15 @@
-print("Mario")
+#Nuestro primer script
+'''
+A
+A
+A
+A
+Este es un comentario largo
+'''
+
+# Entrada de datos se realiza con input
+# Salida de datos se realiza con print
+print('Formulario')
+print('Ingresa tu nombre: ')
+nombre = input()
+print('El nombre ingresado es: ',nombre)
