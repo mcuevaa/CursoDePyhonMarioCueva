fe714aaeb3747d89039a2e59b464e1fe56f5d1f1
Mario <mecuevaa@gmail.com>
2022-02-11 11:23:27 -0500

    inicio

---
+++ Fundamntos/1.InputOutput.py
@@ -0,0 +1,23 @@
+# Función entrada y función de salida
+# Va a buscar las palabras reservas
+
+#Argumento a la función, '' -> string
+print('Formulario')
+print('Nombre: ', end='')
+# Input -> entrada datos
+nombre = input()
+print('Apellido: ', end='')
+apellido = input()
+print('Edad: ', end='')
+edad = input()
+
+# Primer reto curso, implicita print
+ciudad = input('Ingresa tu ciudad: ')
+
+print('El nombre ingresado es: ',nombre)
+print('El apellido ingresado es: ',apellido)
+print('Tu edad ingresada es: ',edad)
+print('Tu ciudad ingresada es: ',ciudad)
+
+
+# Python es un lenguaje 100% orientado a objetos
