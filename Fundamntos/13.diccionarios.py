a2e342bde64a7348a7879fd9229c18e104f91328
Mario <mecuevaa@gmail.com>
2022-02-22 20:29:21 -0500

    yes

---
+++ Fundamntos/13.diccionarios.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+diccionario1 ={
+    'i1': 1,
+    'i2': [1,2,True, 'Mario']
+}
+
+print(type(diccionario1))
+print(diccionario1)
+
+# Acceder a los datos de un diccionario
+print(diccionario1['i2'])
+
+
+
+persona = {
+    'nombre': 'Mario',
+    'apellido': 'Cueva',
+    'edad': 22,
+    'casado': False,
+    21: True
+}
+
+print(type(persona))
+print(persona['edad'])
