6b421b9c5be9588fa94083dbc9fe7996a5abae05
Mario <mecuevaa@gmail.com>
2022-03-04 19:49:21 -0500

    ok

---
+++ Fundamntos/14.conjuntos.py
@@ -0,0 +1,12 @@
+conjunto1 = {1,4,3,2}
+print(conjunto1)
+print(type(conjunto1))
+
+listaEjemplo = [14,15,16,63,63,89,14]
+conjunto3 = list(set(listaEjemplo))
+print(conjunto3)
+
+conjunto4 = {'Andrés', 'Briyan', 'Andrés', 'Mario'}
+print(conjunto4)
+conjunto1.add(8)
+print(conjunto1)
---
+++ Fundamntos/POO/1.Introduccion.py
@@ -0,0 +1,22 @@
+# POO
+# Prototipo, planilla
+
+# Clase
+# Atributos (características y métodos)
+# ADT -> Abstrac Data Type
+
+class Curso:
+    # Atributos
+    # Métodos
+    pass
+
+# Instanciar una clase -> objeto
+curso1 = Curso()
+curso2 = Curso()
+print(type(curso1))
+
+cursos = [curso1, curso2]
+print(cursos)
+
+
+print('Comprobar si curso1 es instancia de Curso: ',isinstance(curso1, Curso))
